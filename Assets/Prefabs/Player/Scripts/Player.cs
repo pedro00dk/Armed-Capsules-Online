babb8a908e8f5304ff69b12d7706863b7bb8b443
Pedro Henrique <pedro00dk@live.com>
2016-03-19 19:16:57 -0300

    Create the Player script

---
+++ Assets/Prefabs/Player/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Player : MonoBehaviour {
+
+    public float health;
+
+    // Internal properties
+
+    float currentHealth;
+
+	void Start () {
+        currentHealth = health;
+	}
+}
