92d87e6d61b253bac37641594a195a81ca43b60e
Pedro Henrique <pedro00dk@live.com>
2016-03-19 19:37:18 -0300

    Create PlayerController script
    The PlayerController script controls the player movement.

---
+++ Assets/Prefabs/Player/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+
+    [Header("Player movement properties")]
+    public float moveSpeed;
+
+    // Internal properties
+
+    Rigidbody body;
+    Vector3 movementInput;
+
+    void Start () {
+        body = GetComponent<Rigidbody>();
+        movementInput = new Vector3();
+	}
+	
+	void Update () {
+
+        // Movement input
+        movementInput.Set(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        movementInput *= moveSpeed;
+	}
+
+    void FixedUpdate() {
+
+        // Movement input
+        body.MovePosition(body.position + movementInput * Time.fixedDeltaTime);
+    }
+}
