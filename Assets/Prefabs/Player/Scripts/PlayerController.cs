4c7a46e41dd8501aa897dd8cfe57911c4ec31041
Pedro Henrique <pedro00dk@live.com>
2016-03-19 20:05:30 -0300

    Implement camera movimentation
    The camera moves according the mouse movimentation, the movement behaviour too has been corrected.

--- Assets/Prefabs/Player/Scripts/PlayerController.cs
+++ Assets/Prefabs/Player/Scripts/PlayerController.cs
@@ -2,32 +2,51 @@
 using System.Collections;
 
 [RequireComponent(typeof(Player))]
+[RequireComponent(typeof(Camera))]
 [RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour {
 
     [Header("Player movement properties")]
-    public float moveSpeed;
+    public float movementSpeed;
+
+    [Header("Player camera properties")]
+    public Camera cam;
+    public Vector2 cameraSensitivity;
+    public float minimumVerticalAngle;
+    public float maximumVerticalAngle;
 
     // Internal properties
 
     Rigidbody body;
+
     Vector3 movementInput;
+    Vector3 movementSmoothInput;
+    Vector3 movementVelocity;
+
+    Vector2 cameraInput;
 
     void Start () {
         body = GetComponent<Rigidbody>();
-        movementInput = new Vector3();
 	}
 	
 	void Update () {
 
         // Movement input
-        movementInput.Set(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
-        movementInput *= moveSpeed;
-	}
+        movementInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical")) * movementSpeed;
+        movementSmoothInput = Vector3.SmoothDamp(movementSmoothInput, movementInput, ref movementVelocity, 0.2f);
+
+        // Camera input
+        cameraInput = new Vector2(
+            Input.GetAxis("Mouse X") * cameraSensitivity.x,
+            Mathf.Clamp(cameraInput.y + Input.GetAxis("Mouse Y") * cameraSensitivity.y, minimumVerticalAngle, maximumVerticalAngle)
+        );
+        transform.Rotate(new Vector3(0, cameraInput.x, 0));
+        cam.transform.localEulerAngles = new Vector3(-cameraInput.y, 0, 0);
+    }
 
     void FixedUpdate() {
 
         // Movement input
-        body.MovePosition(body.position + movementInput * Time.fixedDeltaTime);
+        body.MovePosition(body.position + transform.TransformVector(movementSmoothInput) * Time.fixedDeltaTime);
     }
 }
