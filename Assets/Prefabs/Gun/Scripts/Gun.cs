96e1a3dbecf9ace1a8746089f95089cdc5ae91d8
Pedro Henrique <pedro00dk@live.com>
2016-03-20 03:26:04 -0300

    Create Gun script with some properties
    The gun does not shoot and the player cannot hold a gun yet.

---
+++ Assets/Prefabs/Gun/Scripts/Gun.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour {
+
+    [Header("Gun properties")]
+    public FireMode fireMode;
+    public float shootDamage;
+    public float shootSpeed;
+    public float fireRate;
+    public int magazineCapacity;
+    public float reloadTime;
+    [Space]
+    public int burstCapacity;
+    [Space]
+    public Transform[] barrel;
+
+    // Internal properties
+
+    int currentMagazineCapacity;
+    int currentBurstCapacity;
+
+    void Start () {
+        currentMagazineCapacity = magazineCapacity;
+        currentBurstCapacity = burstCapacity;
+	}
+
+    public void HoldTrigger() {
+
+    }
+
+    public void ReleaseTrigger() {
+
+    }
+
+    void Shoot() {
+
+    }
+
+    public enum FireMode {
+        AUTO,
+        BURST,
+        SINGLE
+    }
+}
