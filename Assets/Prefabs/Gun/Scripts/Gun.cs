3d89b7368efc229b0316be41ed060abb02ac4528
Pedro Henrique <pedro00dk@live.com>
2016-03-20 03:48:15 -0300

    Create the PlayerGunHolder script
    This script allows  the Player hold a Gun object in a setted transform position (normally inside the player), the script too catches the player input to shoot and reload the gun.
    Obs.: The reload function wass added tho the Gun script.

--- Assets/Prefabs/Gun/Scripts/Gun.cs
+++ Assets/Prefabs/Gun/Scripts/Gun.cs
@@ -33,6 +33,10 @@ public class Gun : MonoBehaviour {
 
     }
 
+    public void Reload() {
+
+    }
+
     void Shoot() {
 
     }
---
+++ Assets/Prefabs/Player/Scripts/PlayerGunHolder.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(PlayerController))]
+public class PlayerGunHolder : MonoBehaviour {
+
+    public Gun startGunPrefab;
+    public Transform gunHolder;
+
+    // Internal properties
+
+    Gun equippedGun;
+    Camera playerControllerCamera;
+
+    void Start() {
+        playerControllerCamera = GetComponent<PlayerController>().cam;
+        equippedGun = Instantiate(startGunPrefab, gunHolder.position, Quaternion.identity) as Gun;
+        equippedGun.transform.parent = gunHolder;
+    }
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0)) {
+            equippedGun.HoldTrigger();
+        } else if (Input.GetMouseButtonUp(0)) {
+            equippedGun.ReleaseTrigger();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            equippedGun.Reload();
+        }
+    }
+}
