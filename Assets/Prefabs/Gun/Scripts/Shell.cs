05b8194520d018d7bfdfe176f7aafbbe4e522b29
Pedro Henrique <pedro00dk@live.com>
2016-03-21 16:17:19 -0300

    Create Shell script
    This script represents the bullet shells that are ejected when the gun shoot.

---
+++ Assets/Prefabs/Gun/Scripts/Shell.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Shell : MonoBehaviour {
+
+    // Internal properties
+
+    float velocity;
+    float lifeTime;
+
+    Rigidbody body;
+
+    void Awake() {
+        body = GetComponent<Rigidbody>();
+    }
+
+    public void SetProperties(float velocity, float lifeTime) {
+        this.velocity = velocity;
+        this.lifeTime = lifeTime;
+    }
+
+    public void Eject() {
+        body.AddForce(transform.right * velocity, ForceMode.VelocityChange);
+        StartCoroutine(DestroyShell());
+        Destroy(gameObject, lifeTime);
+    }
+
+    IEnumerator DestroyShell() {
+        yield return new WaitForSeconds(lifeTime * 2 / 3);
+
+        float incRate = 1 / (lifeTime / 3);
+        float percent = 0;
+        Vector3 projectileScale = transform.localScale;
+
+        while (percent <= 1) {
+            percent += Time.deltaTime * incRate;
+            transform.localScale = projectileScale * (1 - percent);
+            yield return null;
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        print(col.collider.name);
+    }
+}
